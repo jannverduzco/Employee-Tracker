104684aca61fd5a6ed430bc68f12c580cdbc66ab
Janneth Verduzco <janneth_verduzco@live.com>
2021-01-14 17:59:31 -0600

    created employeeTrackeCode.js and included required packages (mysql, inquirer) as well as the port connection

---
+++ employeeTrackerCode.js
@@ -0,0 +1,26 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// connection object that establishes port 
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "",
+    database: "top_songsDB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    runEmployeePrompt();
+});
+
+function runEmployeePrompt() {
+
+}
