38212e78ab8dbd279b0185d186d2fad3a0f544eb
Janneth Verduzco <janneth_verduzco@live.com>
2021-01-14 23:28:37 -0600

    added runEmployeePrompt function

--- employeeTrackerCode.js
+++ employeeTrackerCode.js
@@ -1,19 +1,15 @@
+// Dependencies
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+// const util = require("util");
 
 // connection object that establishes port 
 var connection = mysql.createConnection({
     host: "localhost",
-
-    // Your port; if not 3306
     port: 3306,
-
-    // Your username
     user: "root",
-
-    // Your password
-    password: "",
-    database: "top_songsDB"
+    password: "Swimm210!",
+    database: "employee_DB;"
 });
 
 connection.connect(function(err) {
@@ -22,5 +18,71 @@ connection.connect(function(err) {
 });
 
 function runEmployeePrompt() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "rawlist",
+            message: "What would you like to do?",
+            choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager"]
+
+        })
+        .then(function(answer) {
+            switch (answer.action) {
+                case "View All Employees":
+                    viewEmployees();
+                    break;
+
+                case "View All Employees By Department":
+                    viewEmployeesByDep();
+                    break;
+
+                case "View All Employees By Manager":
+                    viewEmployeesByMngr();
+                    break;
+
+                case "Add Employee":
+                    addEmployee();
+                    break;
+
+                case "Remove Employee":
+                    removeEmployee();
+                    break;
+
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+
+                case "Update Employee Manager":
+                    updateEmployeeMngr();
+                    break;
+            }
+        })
+}
+
+function viewEmployees() {
+
+}
+
+function viewEmployeesByDep() {
+
+}
+
+function viewEmployeesByMngr() {
+
+}
+
+function addEmployee() {
+
+}
+
+function removeEmployee() {
+
+}
+
+function updateEmployeeRole() {
+
+}
+
+function updateEmployeeMngr() {
 
 }
